89db0843cf06cf2e91ffa555337e7609fe06e77d
zfranklyn <zfranklyn@gmail.com>
2016-07-30 18:36:18 +0800

    webstorm

--- db.js
+++ db.js
@@ -2,15 +2,18 @@
 //FUNCTION: Connects to database
 
 var mongoose = require('mongoose');
+var config = require('config');
+
 
 // connects to MLab database
-mongoose.connect('mongodb://zfranklyn:legend123@ds025449.mlab.com:25449/ycei')
+mongoose.connect(config.get("testDB"));
+
 
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
-  console.log("Connected to MongoDB database!")
-})
+    console.log("Connected to database")
+});
 
 module.exports = db;
