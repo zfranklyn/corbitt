5788283c11f7edab1311253d63fa0e5ac1d16173
Franklyn Zhu <zfranklyn@gmail.com>
2016-10-12 09:44:00 -0400

    updated schedule

--- routes/complete.js
+++ routes/complete.js
@@ -5,49 +5,45 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db.js');
-var twilio = require('../twilio.js');
+var twilio = require('../twilio.js').twilio;
 var messages = require('../messages');
 var misc = require('../misc.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     var date = misc.date();
-    var userID = Number(req.query.id);
+    var userID = Number(req.query.number);
     var surveyType = req.query.survey;
-    console.log("userID,", userID);
+
     // find the user that completed the survey
-    db.returnUserBasedOnID(userID).then(function (user) {
+    db.findUser(userID).then(function (user) {
         var userTel = user.number;
-        // sanity check: user should not have completed this survey twice today
-        // sometimes Qualtrics sends multiple GET requests; this is to ensure
-        // the user doesn't get spam SMS
-        if (user.completed == false) {
-            console.log(date + ": USER " + userID + " completed today's survey");
-            twilio.sendMessage(userTel, messages.completedSurveyReply);
+        db.returnUserBasedOnID(userID).then(function (user) {
+            // sanity check: user should not have completed this survey twice today
+            // sometimes Qualtrics sends multiple GET requests; this is to ensure
+            // the user doesn't get spam SMS
+            if (user.completed == false) {
+                console.log(date + ": USER " + userID + " completed today's survey");
+                twilio.sendMessage(userTel, messages.completedSurveyReply);
 
-            // redefine current user object:
-            user.completed = true;
-            user.completionTime = misc.timeRightNow();
-            user.save(function (error) {
+                // redefine current user object:
+                user.completed = true;
+                user.save(function (error) {
 
-                if (!error) {
-                    console.log("USER " + userID + " has completed survey; db records have been updated");
-                } else {
-                    // if for some reason...
-                    console.log("USER " + userID + "has completed survey, but records have FAILED to be updated");
-                    twilio.sendMessage(messages.adminNumber, "USER " + userID + "completed survey, but db records failed to update");
-                }
-            })
-        } else {
-            console.log("USER " + userTel + " has already completed this survey.")
-        }
-    }).catch(function(err){
-        console.log(err);
+                    if (!error) {
+                        console.log("USER " + userID + " has completed survey; db records have been updated");
+                    } else {
+                        // if for some reason...
+                        console.log("USER " + userID + "has completed survey, but records have FAILED to be updated");
+                        twilio.sendMessage(messages.adminNumber, "USER " + userID + "completed survey, but db records failed to update");
+                    }
+                })
+            } else {
+                console.log("USER " + userTel + " has already completed this survey.")
+            }
+        });
     });
-
-    res.sendStatus(200);
-
-})
+});
 
 
 module.exports = router;
